99c86cd08d7ff2229369442eaf3b33b41eab988b
painful_pig <576772193@qq.com>
2023-03-08 17:22:15 +0800

    feat: complete auto deploy test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Welcome to pigtest-cli home page.</h1>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -16,6 +16,9 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/test-deploy/dist'
-})
+	transpileDependencies: true,
+	publicPath: "/test-deploy/dist",
+	configureWebpack: {
+		plugins: [
+			AutoImport({
+				resolvers: [ElementPlusResolver()],
+			}),
+			Components({
+				resolvers: [ElementPlusResolver()],
+			}),
+		],
+	},
+});
